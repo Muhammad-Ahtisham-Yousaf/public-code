9c1e5134d9f674b9b5386fa58041f8779ba533ce
Muhammad-Ahtisham-Yousaf <157544539+Muhammad-Ahtisham-Yousaf@users.noreply.github.com>
2024-02-29 18:14:33 -0800

    Update password_edit_form.py

--- app/djangoForms/password_edit_form.py
+++ app/djangoForms/password_edit_form.py
@@ -1,36 +1,55 @@
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User
 
+from django import forms
+from ..models import UserDetails
+# from djangoForms.password_edit_form import UserDetailsForm
 
 class UserChangeCustomForm(UserChangeForm):
+    specialization = forms.CharField(max_length =200, widget=forms.TextInput(attrs={ 'placeholder': 'Dspecialization', 'class': ''}) )
+    location = forms.CharField(max_length=255, widget=forms.TextInput(attrs={ 'placeholder': 'Dlocation', 'class': ''})  )
 
 
     class Meta:
-        model = User
-        fields = ('first_name','username','email',)
-        # fields = ('__all__')
+        model = UserDetails
+        # fields = ('specialization','first_name','username','email')
+        fields = ('__all__')
+        exclude = ('user','type') 
 
-from django import forms
-from ..models import EditUser
-
-class EditUserForm (forms.ModelForm):      
+        
+class UserDetailsForm (forms.ModelForm):      
     location = forms.CharField(max_length=255, widget=forms.TextInput(attrs={ 'placeholder': 'Dlocation', 'class': ''})  )
     mobile_no = forms.CharField(max_length=255,widget=forms.TextInput(attrs={ 'placeholder': 'Dmobile', 'class': ''}) )
     about_me = forms.CharField( widget=forms.Textarea(attrs={ 'placeholder': 'Dabout', 'class': ''}) )
     skills = forms.CharField(max_length=200, widget=forms.TextInput(attrs={ 'placeholder': 'Dskills', 'class': ''}) )
-    joining_date = forms.CharField(max_length=100, widget=forms.TextInput(attrs={ 'placeholder': 'Djoining date', 'class': ''}) )
+    # date = forms.CharField(max_length=100, widget=forms.TextInput(attrs={ 'placeholder': 'Djoining date', 'class': ''}) )
     education = forms.CharField(max_length=255, widget=forms.TextInput(attrs={ 'placeholder': 'Deducation', 'class': ''}) )
  #     company_logo = forms.ImageField,upload_to='app' ,default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5RhqcwxGDJustOfPZ5ybmAWzysfuMvTAYoUyVx63gKBwNRIeMm9kDwlS5NEN6I6vrnac&usqp=CAU' )
     university = forms.CharField(max_length =66, widget=forms.TextInput(attrs={ 'placeholder': 'Duniverstiy', 'class': ''}) )
     priveus_job = forms.CharField(max_length=70, widget=forms.TextInput(attrs={ 'placeholder': 'Dprivious job', 'class': ''}) )
-    projects = forms.CharField(max_length =200, widget=forms.Textarea(attrs={ 'placeholder': 'Dprojects', 'class': ''}) )
-    project_desc = forms.CharField(max_length =200, widget=forms.Textarea(attrs={ 'placeholder': 'Dprojectd', 'class': ''}) )
+    # projects = forms.CharField(max_length =200, widget=forms.Textarea(attrs={ 'placeholder': 'Dprojects', 'class': ''}) )
+    specialization = forms.CharField(max_length =200, widget=forms.TextInput(attrs={ 'placeholder': 'Dspecialization', 'class': ''}) )
     interests = forms.CharField(max_length =200, widget=forms.TextInput(attrs={ 'placeholder': 'Dinterests', 'class': ''}) )
     gender = forms.CharField(max_length =200, widget=forms.TextInput(attrs={ 'placeholder': 'Dgender', 'class': ''}) )
- 
-    class Meta:
-        model = EditUser
-        # fields = ('first_name','username','email',)
-        fields = ('__all__')
-
     
+    class Meta:
+        model = UserDetails
+        # fields = ('location','mobile_no','about_me','skills','','about_me',)
+        fields = ('__all__')
+        exclude = ('username','password','date_joined') 
+
+
+from django import forms
+from ..models import EmployerDetails
+
+class EmployerDetailsForm (forms.ModelForm):      
+    industry = forms.CharField(max_length=255, widget=forms.TextInput(attrs={ 'placeholder': 'Dindustry', 'class': ''})  )
+    company = forms.CharField(max_length=255,widget=forms.TextInput(attrs={ 'placeholder': 'Dcompany', 'class': ''}) )
+    contact_no = forms.CharField( widget=forms.Textarea(attrs={ 'placeholder': 'Dcontact_no', 'class': ''}) )
+    location = forms.CharField(max_length=200, widget=forms.TextInput(attrs={ 'placeholder': 'Dlocation', 'class': ''}) )
+
+    class Meta:
+        model = User
+        # fields = ('location','mobile_no','about_me','skills','','about_me',)
+        fields = ('__all__')
+        exclude = ('username','password','date_joined') 
