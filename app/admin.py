d031de13172bdf5b0623c0c7166fcdab7b07085c
Muhammad-Ahtisham-Yousaf <157544539+Muhammad-Ahtisham-Yousaf@users.noreply.github.com>
2024-02-29 18:23:03 -0800

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
-from .models import JobPosting , UserDetails, EditUser
+from .models import JobPosting , EmployerDetails, UserDetails
 
 class JobPostingAdmin(admin.ModelAdmin):
     list_display = ('id','job_title','job_location','skills_required','posting_date','salary','industry','education','experience_required', 'gender', 'company_name')
 
-class UserDetailsAdmin(admin.ModelAdmin):
-    list_display = ('id','industry','company','contact_no','location','user','type') 
+class EmployerDetailsAdmin(admin.ModelAdmin):
+    list_display = ('id','industry','company','contact_no','location','employer','type') 
 
-admin.site.register(UserDetails,UserDetailsAdmin)
+admin.site.register(EmployerDetails,EmployerDetailsAdmin)
 
 #  Register your models here.
 admin.site.register(JobPosting,JobPostingAdmin)
 # # admin.site.register(Myuser,MyAdmin)
 
-class EditUserAdmin(admin.ModelAdmin):
-    list_display = ('id','location','mobile_no','skills','joining_date','education','university','priveus_job','interests', 'gender')
+class UserDetailsAdmin(admin.ModelAdmin):
+    list_display = ('id','location','mobile_no','skills','specialization','education','university','priveus_job','interests', 'gender','user','type' )
 
 #  Register your models here.
-admin.site.register(EditUser,EditUserAdmin)
+admin.site.register(UserDetails,UserDetailsAdmin)
 # # admin.site.register(Myuser,MyAdmin)
